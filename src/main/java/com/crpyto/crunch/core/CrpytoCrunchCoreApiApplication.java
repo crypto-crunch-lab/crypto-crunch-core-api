7a108ef8f90a1e54612ab1e00c624b66196eccbf
ywoo-park2 <ywoo.park2@navercorp.com>
2022-04-12 00:37:45 +0900

    datasource 설정전에도 build 되도록 처리

--- src/main/java/com/crpyto/crunch/core/CrpytoCrunchCoreApiApplication.java
+++ src/main/java/com/crpyto/crunch/core/CrpytoCrunchCoreApiApplication.java
@@ -1,9 +1,12 @@
 package com.crpyto.crunch.core;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CrpytoCrunchCoreApiApplication {
 
     public static void main(String[] args) {
