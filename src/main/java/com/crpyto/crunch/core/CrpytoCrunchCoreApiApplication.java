e02b52a409544118fce7712930014ced3a0f97df
ywoo-park2 <ywoo.park2@navercorp.com>
2022-04-11 23:54:26 +0900

    initial commit

---
+++ src/main/java/com/crpyto/crunch/core/CrpytoCrunchCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.crpyto.crunch.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrpytoCrunchCoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrpytoCrunchCoreApiApplication.class, args);
+    }
+
+}
