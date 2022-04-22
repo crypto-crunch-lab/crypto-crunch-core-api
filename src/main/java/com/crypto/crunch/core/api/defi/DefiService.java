ce8e3a92773ac4321c5b51898db54817793dc29b
ywoo-park2 <ywoo.park2@navercorp.com>
2022-04-23 01:14:57 +0900

    feat: 디파이 어드민 관련 비즈니스 로직 추가

--- src/main/java/com/crypto/crunch/core/api/defi/DefiService.java
+++ src/main/java/com/crypto/crunch/core/api/defi/DefiService.java
@@ -5,8 +5,11 @@ import com.crypto.crunch.core.domain.defi.DefiRequest;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public interface DefiService {
     List<Defi> search(DefiRequest request) throws Exception;
     List<String> getNetworks() throws IOException;
+    Map<String, Map<String, String>> getAdminMetaFields();
+    void update(Defi defi) throws IOException;
 }
